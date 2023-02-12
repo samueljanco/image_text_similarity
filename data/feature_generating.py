af7e10f8ce50c1b75036a1044522e64bc3134222
samueljanco <124911218+samueljanco@users.noreply.github.com>
2023-02-12 14:20:00 +0100

    Update feature_generating.py
    adding function to save text embeddings

--- data/feature_generating.py
+++ data/feature_generating.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import numpy as np
 from image_features import  ImageEncoder
 
@@ -18,3 +19,29 @@ def save_image_features():
 
     with open('coco_image_embeddings.npy', 'wb') as f:
         np.save(f, image_features)
+
+ 
+def save_text_features():
+    cc_file = np.array(list(csv.reader(
+        open("C:/Users/samoj/Downloads/Crisscrossed-Captions-master/Crisscrossed-Captions-master/data/sits_val.csv"),
+        delimiter=',')))[1:]
+    caption_names = np.unique(cc_file[:, 0])
+    caption_ids = [int(c[20:]) for c in caption_names]
+    json_captions = json.load(open("C:/Users/samoj/Downloads/dataset_coco.json/dataset_coco.json"))
+    caption_texts = []
+    caption_sentids = []
+
+    for img in json_captions['images']:
+        for sentence in img['sentences']:
+            if sentence['sentid'] in caption_ids:
+                caption_sentids.append(sentence['sentid'])
+                caption_texts.append(sentence['raw'])
+
+    caption_sentids = np.array(caption_sentids)
+    text_encoder = TextEncoder()
+    text_features = np.array(text_encoder.encode(caption_texts))
+
+    text_features = np.column_stack((caption_sentids, text_features))
+
+    with open('coco_text_embeddings.npy', 'wb') as f:
+        np.save(f, text_features)
