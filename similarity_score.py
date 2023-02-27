aa280b24c02d614caca30315c6cb48352e41b469
samueljanco <124911218+samueljanco@users.noreply.github.com>
2023-02-27 23:35:22 +0100

    Update similarity_score.py

--- similarity_score.py
+++ similarity_score.py
@@ -6,13 +6,13 @@ from image_features import ImageEncoder
 from text_features import TextEncoder
 
 class SimilarityScore:
-    def __init__(self, combine_via='multiplication'):
+    def __init__(self,init_encoder=False, combine_via='multiplication'):
         image_input = Input(shape=(4096,))
         text_input = Input(shape=(512,))
 
         if combine_via == 'multiplication':
-            x1 = Dense(64, activation='relu')(image_input)
-            x2 = Dense(64, activation='relu')(text_input)
+            x1 = Dense(256, activation='relu')(image_input)
+            x2 = Dense(256, activation='relu')(text_input)
             x = keras.layers.multiply([x1, x2])
         elif combine_via == 'shared_layer':
             shared_dense_layer = keras.layers.Dense(64, activation='relu')
@@ -24,16 +24,17 @@ class SimilarityScore:
             x_2 = keras.layers.Dense(64, activation='relu')(text_input)
             x = keras.layers.concatenate([x_1, x_2])
 
-        x = keras.layers.Dense(32, activation='relu')(x)
+        x = keras.layers.Dense(128, activation='relu')(x)
         outputs = keras.layers.Dense(1, activation='linear')(x)
 
         self.model = Model(inputs=[image_input, text_input], outputs=outputs)
         self.model.compile(optimizer='adam', loss='mean_squared_error')
-        self.image_encoder = ImageEncoder()
-        self.text_encoder = TextEncoder()
+        if init_encoder:
+            self.image_encoder = ImageEncoder()
+            self.text_encoder = TextEncoder()
 
     def fit(self, image_features, text_features, target):
-        self.model.fit([image_features, text_features], target, epochs=50, batch_size=32)
+        self.model.fit([image_features, text_features], target, epochs=70, batch_size=32)
 
     def predict_from_features(self, image_feature, text_feature):
         return self.model.predict([image_feature, text_feature])
