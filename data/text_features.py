d4a6b0bf1b08f7c0b2f5c4cd181e1fb7ebff0f1c
samueljanco <124911218+samueljanco@users.noreply.github.com>
2023-02-27 17:11:01 +0100

    Update text_features.py

--- data/text_features.py
+++ data/text_features.py
@@ -11,24 +11,22 @@ class TextEncoder:
 
 
     def encode(self, text_list, batch_size=32):
-
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        tokenized_text = [tokenizer.encode(text, add_special_tokens=True) for text in text_list]
+        
+        tokenized_text = [self.tokenizer.encode(text, add_special_tokens=True) for text in text_list]
 
 
         max_length = max([len(text) for text in tokenized_text])
         padded_text = tf.keras.preprocessing.sequence.pad_sequences(tokenized_text, maxlen=max_length, padding='post')
 
-        bert_model = TFBertModel.from_pretrained('bert-base-uncased')
-
+   
         embeddings = []
         for i in range(0, len(padded_text), batch_size):
             inputs = {
                 'input_ids': padded_text[i:i+batch_size],
                 'attention_mask': tf.where(padded_text[i:i+batch_size] != 0, 1, 0),
-                'token_ids': tf.zeros_like(padded_text[i:i+batch_size])
+                'token_type_ids': tf.zeros_like(padded_text[i:i+batch_size])
             }
-            batch_embeddings = bert_model(inputs)[1].numpy()
+            batch_embeddings = self.bert_model(inputs)[1].numpy()
             embeddings.append(batch_embeddings)
         embeddings = np.concatenate(embeddings, axis=0)
 
